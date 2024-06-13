2138fd148200ce2e84bf96e42ea06a0c04071ed6
xelab04 <alexbissessur@gmail.com>
2024-06-13 22:32:31 +0400

    Added method to find existing nodeports

---
+++ CreateCluster.py
@@ -0,0 +1,55 @@
+import base64
+import subprocess
+
+
+def run_command(command):
+    """Run a shell command and return its output."""
+    result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return result.stdout.decode().strip()
+
+
+def create_vcluster(cluster_name, timeout=40):
+    """Create a vcluster and terminate the command after a timeout."""
+    print(f"Creating vcluster '{cluster_name}'...")
+    proc = subprocess.Popen(f"vcluster create {cluster_name}", shell=True, stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE)
+    try:
+        proc.communicate(timeout=timeout)
+    except subprocess.TimeoutExpired:
+        proc.terminate()
+        print(f"Terminated vcluster creation command after {timeout} seconds.")
+    if proc.returncode == 0:
+        print(f"vcluster '{cluster_name}' created successfully.")
+    else:
+        print(f"vcluster '{cluster_name}' creation command terminated with code {proc.returncode}.")
+
+
+def switch_context():
+    """Switch back to the default context."""
+    print("Switching back to the default cluster context...")
+    run_command("kubectl config use-context default")
+    print("Switched back to the default cluster context.")
+
+
+def get_kubeconfig(cluster_name: str) -> None:
+    """Retrieve and decode the kubeconfig for the vcluster."""
+    print(f"Retrieving kubeconfig for vcluster '{cluster_name}'...")
+    secret_name = f"vc-{cluster_name}"
+    namespace = f"vcluster-{cluster_name}"
+    encoded_kubeconfig = run_command(
+        f"kubectl get secret {secret_name} -n {namespace}" + " --template={{.data.config}}")
+    print(encoded_kubeconfig)
+    decoded_kubeconfig = base64.b64decode(encoded_kubeconfig).decode('utf-8')
+
+    kubeconfig_path = f"./kubeconfig-{cluster_name}"
+    with open(kubeconfig_path, "w") as f:
+        f.write(decoded_kubeconfig)
+
+    print(f"Kubeconfig for vcluster '{cluster_name}' saved to '{kubeconfig_path}'.")
+
+
+def main(cluster_name):
+    #cluster_name = "test-cluster"
+    create_vcluster(cluster_name)
+    switch_context()
+    return get_kubeconfig(cluster_name)
---
+++ GetServices.py
@@ -0,0 +1,29 @@
+from kubernetes import client, config
+import pprint
+import random
+# Configs can be set in Configuration class directly or using helper utility
+
+def get_list_nodes_used():
+    config.load_kube_config()
+    
+    v1 = client.CoreV1Api()
+    print("Listing pods with their IPs:")
+    ret = v1.list_service_for_all_namespaces(watch=False)
+    
+    service_dict = ret.to_dict()
+    pprint.pprint(service_dict.keys())
+
+    nodes_used = [svc.spec.ports[0].node_port for svc in ret.items]
+    return nodes_used
+        
+def main():
+    
+    nodes_used = get_list_nodes_used()
+    while True:
+        new_node_port = random.randint(30005, 32767)
+        if new_node_port not in nodes_used:
+            return new_node_port
+    
+        
+if __name__ == "__main__":
+    main()
